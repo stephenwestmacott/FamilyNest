4c2c47f71a9da3e23c4f2ac3ebff9b8704ec6cae
stephenwestmacott <stephenhwestmacott@gmail.com>
2024-09-25 23:03:39 -0400

    drawer ui cleaned up. cleaned up code to get user profile data for drawer. added profile pic.

--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -2,7 +2,7 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
 import { View } from "@/components/Themed";
 import { DrawerContentScrollView } from "@react-navigation/drawer";
-import { Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
+import { Text, StyleSheet, TouchableOpacity, Alert, Image } from "react-native";
 import { useState, useEffect } from "react";
 import { supabase } from "../lib/supabase";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -15,45 +15,55 @@ type Profile = {
   id: string;
   email: string;
   name: string;
-  // Add other profile fields here
+  current_family: string;
+  profile_picture_url: string;
 };
 
 const CustomDrawerContent = (props: any) => {
   const [profile, setProfile] = useState<Profile | null>(null);
-  const [userID, setUserID] = useState<string | null>(null);
+  const [name, setName] = useState<string | null>(null);
+  const [selectedFamily, setSelectedFamily] = useState<string | null>(null);
+  const [email, setEmail] = useState<string | null>(null);
+  const [profilePictureUrl, setProfilePictureUrl] = useState<string | null>(
+    null
+  );
+
+  const fetchProfileData = async () => {
+    try {
+      const {
+        data: { session },
+        error: sessionError,
+      } = await supabase.auth.getSession();
+      if (sessionError) {
+        Alert.alert("Error Fetching Session", sessionError.message);
+        return;
+      }
+
+      if (session?.user) {
+        const { data: profileData, error } = await supabase
+          .from("Profiles")
+          .select("*")
+          .eq("id", session.user.id)
+          .single();
+
+        if (error) {
+          Alert.alert("Error Fetching Profile", error.message);
+        } else {
+          setSelectedFamily(profileData?.current_family);
+          setProfile(profileData);
+          setName(profileData?.name);
+          setEmail(session.user.email || "");
+          setProfilePictureUrl(profileData?.profile_picture_url);
+        }
+      } else {
+        Alert.alert("User Not Found");
+      }
+    } catch (error) {
+      Alert.alert("Error", "An unexpected error occurred.");
+    }
+  };
 
   useEffect(() => {
-    const fetchProfileData = async () => {
-      try {
-        const {
-          data: { session },
-          error: sessionError,
-        } = await supabase.auth.getSession();
-        if (sessionError) {
-          Alert.alert("Error Fetching Session", sessionError.message);
-          return;
-        }
-
-        if (session?.user) {
-          const { data: profileData, error } = await supabase
-            .from("Profiles")
-            .select("*")
-            .eq("id", session.user.id)
-            .single();
-
-          if (error) {
-            Alert.alert("Error Fetching Profile", error.message);
-          } else {
-            setProfile(profileData as Profile);
-          }
-        } else {
-          Alert.alert("User Not Found");
-        }
-      } catch (error) {
-        Alert.alert("Error", "An unexpected error occurred.");
-      }
-    };
-
     fetchProfileData();
   }, []);
 
@@ -63,10 +73,8 @@ const CustomDrawerContent = (props: any) => {
       if (error) {
         Alert.alert("Error Signing Out", error.message);
       } else {
-        // Clear AsyncStorage to remove any persisted sessions
         await AsyncStorage.clear();
         console.log("User signed out and AsyncStorage cleared");
-        // Redirect to the login screen after sign-out
       }
     } catch (error) {
       Alert.alert("Error", "An unexpected error occurred while signing out.");
@@ -77,12 +85,17 @@ const CustomDrawerContent = (props: any) => {
     <DrawerContentScrollView {...props}>
       <SafeAreaView style={{ flex: 1 }}>
         <Stack.Screen options={{ headerShown: true, title: "Settings" }} />
-        <View style={{ padding: 16 }}>
-          <Text>Profile:</Text>
-          <Text>
-            {profile ? JSON.stringify(profile, null, 2) : "Loading profile..."}
+        <View style={styles.profileContainer}>
+          <Image
+            source={{ uri: profilePictureUrl || "" }}
+            style={styles.profileImage}
+          />
+          <Text style={styles.profileText}>Name: {name || ""}</Text>
+          <Text style={styles.profileText}>Email: {email || ""}</Text>
+          <Text style={styles.profileText}>
+            Selected Family: {selectedFamily || "hi"}
           </Text>
-          <QrCode familyID={profile?.id || "hi"} />
+          <QrCode familyID={selectedFamily || "hi"} />
           <TouchableOpacity onPress={doLogout} style={styles.buttonContainer}>
             <Text style={styles.buttonText}>LOGOUT</Text>
           </TouchableOpacity>
@@ -109,18 +122,34 @@ export default function Layout() {
 }
 
 const styles = StyleSheet.create({
+  profileContainer: {
+    padding: 16,
+    alignItems: "center",
+  },
+  profileImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom: 16,
+  },
+  profileText: {
+    fontSize: 16,
+    color: "#333",
+    marginBottom: 8,
+  },
   buttonContainer: {
-    backgroundColor: "#000968",
+    backgroundColor: "#e8b9b4",
     borderRadius: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 12,
-    margin: 8,
+    paddingVertical: 12,
+    paddingHorizontal: 16,
+    marginTop: 16,
+    width: "100%",
   },
   buttonText: {
     fontSize: 18,
     color: "#fff",
     fontWeight: "bold",
-    alignSelf: "center",
+    textAlign: "center",
     textTransform: "uppercase",
   },
 });
